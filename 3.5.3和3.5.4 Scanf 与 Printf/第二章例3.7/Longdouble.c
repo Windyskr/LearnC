92ea5b420c2be7367d0ae950d684abe4712b64e0
Yan <oyyfcy@outlook.com>
2021-10-15 11:11:52 +0800

    double 在 printf 和 scanf 的解释

--- 3.5.3和3.5.4 Scanf 与 Printf/第二章例3.7/Longdouble.c
+++ 3.5.3和3.5.4 Scanf 与 Printf/第二章例3.7/Longdouble.c
@@ -2,9 +2,8 @@
 int main()
 {
     long double a;
-    //直接 double /Long Double 就可以了，不是 double float
-    a = 10000 / 3, 0;
+    // 直接 double /Long Double 就可以了，不是 double float
+    a = 10000.0 / 3, 0;
     printf("%Lf\n", a);
-    // Lf 是大写的 L
-    return 0;
+    // scanf中%f对应float，%lf对应double；printf中因为float会自动提升类型到double，所以只需要%f就行了。Longdouble 才是 Lf。    return 0;
 }
