b716b6f2aede4867fe24f8c040a2cb799ff5d8cf
Yan <oyyfcy@outlook.com>
2021-10-18 17:15:19 +0800

    fix

--- Exercises/个十百单独/3.c
+++ Exercises/个十百单独/3.c
@@ -4,7 +4,7 @@ int main()
     int san, b, s, g;
     scanf("%d", &san);
     g = san % 10;
-    s = san / 10 & 10;
+    s = san / 10 % 10;
     b = san / 100 % 10;
     printf("百位数是%d,十位数是%d,个位数是%d。\n", b, s, g);
     return 0;
