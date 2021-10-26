f99b3d608d7ffd096930c853336aed1282980943
Yan <oyyfcy@outlook.com>
2021-10-26 19:02:32 +0800

    第四章习题

--- 第四章习题/4-12/4-12.c
+++ 第四章习题/4-12/4-12.c
@@ -2,18 +2,19 @@
 #include <math.h>
 int main()
 {
-    float x=2 , y=2, ix, iy, rx, ry;
-    printf ("请输入坐标(x,y):");
-    scanf ("(%f,%f)", &ix, &iy);
-    rx = abs(ix);
-    ry = abs(iy);
-    if (rx < )
+    float ix, iy, rx, ry, distance;
+    printf("请输入坐标(x,y):");
+    scanf("(%f,%f)", &ix, &iy);
+    rx = fabsf(ix);
+    ry = fabsf(iy);
+    distance = sqrt(pow(rx - 2, 2) + pow(ry - 2, 2));
+    if (distance < 1)
     {
-        
+        printf("建筑高度为10。n");
     }
     else
     {
-
+        printf("建筑高度为零。\n");
     }
-
+    return 0;
 }
