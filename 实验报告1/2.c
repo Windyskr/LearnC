fecb186aef5a0bbf5ded9521612e5d6e2060ba8e
Yan <oyyfcy@outlook.com>
2021-10-27 21:37:18 +0800

    注意括号

--- 实验报告1/2.c
+++ 实验报告1/2.c
@@ -6,7 +6,7 @@ int main()
     float a, b, c, deta, x, x1, x2, xubu, shibu;
     printf("请输入三个实数：");
     scanf("%f,%f,%f", &a, &b, &c);
-    deta = pow(b, 2) - 4 * a * c;
+    deta = pow(b, 2) - (4 * a * c);
     if (a == 0)
     {
         if (b == 0)
@@ -16,7 +16,7 @@ int main()
         else
         {
             x = (-b) / c;
-            printf("这是一个一次函数。函数的解为%f\n", x);
+            printf("这是一个一次函数。函数的解为%.2f\n", x);
         }
     }
     else
@@ -24,15 +24,27 @@ int main()
 
         if (deta < 0)
         {
-            xubu = sqrt(-deta) / (2 * a);
+            xubu = fabs(sqrt(-deta) / (2 * a));
             shibu = -b / (2 * a);
-            printf("这个二次函数有两个虚根\n一个是 %f+%fi,另一个是 %f-%fi\n ", shibu, xubu, shibu, xubu);
+            if (shibu == 0)
+            {
+                printf("这个二次函数有两个虚根\n一个是 %.2fi,另一个是 %.2fi\n ", xubu, xubu);
+            }
+            else
+            {
+                printf("这个二次函数有两个虚根\n一个是 %.2f+%.2fi,另一个是 %.2f-%.2fi\n ", shibu, xubu, shibu, xubu);
+            }
+        }
+        else if (deta == 0)
+        {
+            x = (-b + sqrt(deta)) / (2 * a);
+            printf("这个二次函数只有一个实根\n根是%.2f\n", x);
         }
         else
         {
-            x1 = (-b + sqrt(deta)) / 2 * a;
-            x2 = (-b - sqrt(deta)) / 2 * a;
-            printf("这个二次函数有两个实根\n一个根是%f,另一个根是%f\n", x1, x2);
+            x1 = (-b + sqrt(deta)) / (2 * a);
+            x2 = (-b - sqrt(deta)) / (2 * a);
+            printf("这个二次函数有两个实根\n一个根是%.2f,另一个根是%.2f\n", x1, x2);
         }
     }
     return 0;
