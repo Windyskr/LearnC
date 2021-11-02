80299314cffc62331a9d1316292d55ec96d0c631
Yan <oyyfcy@outlook.com>
2021-11-02 22:48:45 +0800

    增加负数报错逻辑同时美化代码

--- 第五章/Book/Prime/isprime.c
+++ 第五章/Book/Prime/isprime.c
@@ -2,19 +2,24 @@
 #include <math.h>
 #include <stdio.h>
 int main() {
-    int n, i, flag = 0;
+    int n, i, flag;
     printf("请输入一个整数：");
     scanf("%d", &n);
-    for (i = 2; i < sqrt(n); i++) {
-        if (n % i == 0) {
-            flag = 1;
-            break;
+    if (n >= 0) {
+        for (i = 2, flag = 0; i < sqrt(n); i++) {
+            if (n % i == 0) {
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == 1) {
+            printf("不是素数\n");
+        } else {
+            printf("是素数\n");
         }
     }
-    if (flag == 1) {
-        printf("不是素数\n");
-    } else {
-        printf("是素数\n");
+    else {
+        printf("输入错误！\n");
     }
     return 0;
 }
