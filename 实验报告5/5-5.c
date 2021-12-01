a5d7f5b1ff2252789db99d2ec1362e9d4ae11b7c
Yan <oyyfcy@outlook.com>
2021-12-01 13:26:25 +0800

    最终版本

--- 实验报告5/5-5.c
+++ 实验报告5/5-5.c
@@ -33,7 +33,7 @@ int main() {
         int i = 0, j = (n - 1) / 2;
         // ①将“1”放在第一行的中间一列；
         arr[i][j] = 1;
-        printf("arr[%d][%d] = 1\n", i, j);
+        // printf("arr[%d][%d] = 1\n", i, j);
         while (sz <= n * n) {
             // 横着行 i， 竖这列 j
             // ②从“2”开始直到n×n为止的各数依次按下列规则存放：
@@ -42,36 +42,36 @@ int main() {
             // ④当上一个数的列数为n，则下一个数的列数为1；
             // ⑤如果按上面规则确定的位置上已有数，或上一个数是第1行第n列时，则把下一个数放在上一个数的下面，
             if ((i == 0 && j == n - 1)) {  // (1,n) 放下面
-                printf("1\n");
+                // printf("1\n");
                 i = i + 1;
                 j = j;
             } else if ((j == n - 1) && (arr[i - 1][0] == 0)) {  // (x,n)
-                printf("2\n");
+                // printf("2\n");
                 i = i - 1;                                      // 行数减1
                 j = 0;                                          // 列数是 1
             } else if ((i == 0) && (arr[n - 1][j + 1] == 0)) {  //(1, x)
-                printf("3\n");
+                // printf("3\n");
                 i = n - 1;  // 行 n
                 j = j + 1;  //列 +1
             } else if (arr[i - 1][j + 1] == 0) {
-                printf("4\n");
+                // printf("4\n");
                 i = i - 1;
                 j = j + 1;
             } else {
-                printf("5\n");
+                // printf("5\n");
                 i = i + 1;
                 j = j;
             }
             arr[i][j] = sz;
-            printf("arr[%d][%d] = %d\n", i, j, sz);
+            // printf("arr[%d][%d] = %d\n", i, j, sz);
             sz++;
-            // 输出测试下
-            for (int ix = 0; ix < n; ix++) {
-                for (int jx = 0; jx < n; jx++) {
-                    printf(" %3d", arr[ix][jx]);
-                }
-                printf("\n");
+        }
+        // 输出测试下
+        for (int ix = 0; ix < n; ix++) {
+            for (int jx = 0; jx < n; jx++) {
+                printf(" %3d", arr[ix][jx]);
             }
+            printf("\n");
         }
     }
     return 0;
