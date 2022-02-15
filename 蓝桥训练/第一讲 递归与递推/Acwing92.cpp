36bc24f1da90c5dbe3ba8e5e94b7d41bfe876313
Yan <oyyfcy@outlook.com>
2022-02-15 16:00:30 +0800

    开始系统学习蓝桥杯

---
+++ 蓝桥训练/第一讲 递归与递推/Acwing92.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+using namespace std;
+int n;        // n 是数字
+int arr[16];  // 表示状态0=未判断 1=选择 2=不选择
+void dfs(int u) {
+    // 如果递归完了那就全部输出
+    if (u > n) {
+        for (int i = 1; i <= n; i++) {
+            if (arr[i] == 1) {
+                printf("%d", i);
+                if (i != n) printf(" ");
+            }
+        }
+        printf("\n");
+        return;
+    } else {
+        // 如果没有递归完呢
+
+        // 先不选
+        arr[u] = 2;
+        dfs(u + 1);
+        arr[u] = 0;
+
+        // 然后是选
+        arr[u] = 1;
+        dfs(u + 1);
+        arr[u] = 0;
+    }
+}
+int main() {
+    cin >> n;
+    dfs(1);
+    return 0;
+}
